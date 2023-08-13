7fe3a2d9d06442bf221e67dba9bad29ae3e66172
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-13 18:43:09 +0530

    main.tf

---
+++ module/main.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "instance1" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t3.micro"
+  count         = 1
+  key_name = "TF_key"
+  tags = {
+    Name = "uat"
+
+  }
+}
+
+resource "aws_key_pair" "TF_key" {
+  key_name   = "TF_key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+
+
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+resource "local_file" "TF_key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tfkey"
+}
